0a8c22da138a7fe825f5865e197fc4bf9f66a251
Sriram Nair <srirambnair@gmail.com>
2026-01-02 20:43:14 -0500

    Fixing DistanceCalculator
    /distance command now returns a value that can be stored in a score (the spherical distance)

--- src/main/java/carpet/utils/DistanceCalculator.java
+++ src/main/java/carpet/utils/DistanceCalculator.java
@@ -19,45 +19,63 @@ public class DistanceCalculator
 
     public static List<Component> findDistanceBetweenTwoPoints(Vec3 pos1, Vec3 pos2)
     {
-        double dx = Mth.abs((float)pos1.x-(float)pos2.x);
-        double dy = Mth.abs((float)pos1.y-(float)pos2.y);
-        double dz = Mth.abs((float)pos1.z-(float)pos2.z);
-        double manhattan = dx+dy+dz;
-        double spherical = Math.sqrt(dx*dx + dy*dy + dz*dz);
-        double cylindrical = Math.sqrt(dx*dx + dz*dz);
+        double dx = Mth.abs((float)pos1.x - (float)pos2.x);
+        double dy = Mth.abs((float)pos1.y - (float)pos2.y);
+        double dz = Mth.abs((float)pos1.z - (float)pos2.z);
+
+        double manhattan = dx + dy + dz;
+        double spherical = Math.sqrt(dx * dx + dy * dy + dz * dz);
+        double cylindrical = Math.sqrt(dx * dx + dz * dz);
+
         List<Component> res = new ArrayList<>();
-        res.add(Messenger.c("w Distance between ",
-                Messenger.tp("c",pos1),"w  and ",
-                Messenger.tp("c",pos2),"w :"));
+        res.add(Messenger.c(
+                "w Distance between ",
+                Messenger.tp("c", pos1),
+                "w  and ",
+                Messenger.tp("c", pos2),
+                "w :"
+        ));
         res.add(Messenger.c("w  - Spherical: ", String.format("wb %.2f", spherical)));
         res.add(Messenger.c("w  - Cylindrical: ", String.format("wb %.2f", cylindrical)));
         res.add(Messenger.c("w  - Manhattan: ", String.format("wb %.1f", manhattan)));
         return res;
     }
 
+    private static int sphericalDistance(Vec3 pos1, Vec3 pos2)
+    {
+        double dx = pos1.x - pos2.x;
+        double dy = pos1.y - pos2.y;
+        double dz = pos1.z - pos2.z;
+        return (int)Math.round(Math.sqrt(dx * dx + dy * dy + dz * dz));
+    }
+
     public static int distance(CommandSourceStack source, Vec3 pos1, Vec3 pos2)
     {
+        int spherical = sphericalDistance(pos1, pos2);
         Messenger.send(source, findDistanceBetweenTwoPoints(pos1, pos2));
-        return 1;
+        return spherical;
     }
 
     public static int setStart(CommandSourceStack source, Vec3 pos)
     {
         START_POINT_STORAGE.put(source.getTextName(), pos);
-        Messenger.m(source,"gi Initial point set to: ", Messenger.tp("g",pos));
+        Messenger.m(source, "gi Initial point set to: ", Messenger.tp("g", pos));
         return 1;
     }
 
     public static int setEnd(CommandSourceStack source, Vec3 pos)
     {
-        if ( !hasStartingPoint(source) )
+        if (!hasStartingPoint(source))
         {
             START_POINT_STORAGE.put(source.getTextName(), pos);
-            Messenger.m(source,"gi There was no initial point for "+source.getTextName());
-            Messenger.m(source,"gi Initial point set to: ", Messenger.tp("g",pos));
+            Messenger.m(source, "gi There was no initial point for " + source.getTextName());
+            Messenger.m(source, "gi Initial point set to: ", Messenger.tp("g", pos));
             return 0;
         }
-        Messenger.send(source, findDistanceBetweenTwoPoints( START_POINT_STORAGE.get(source.getTextName()), pos));
-        return 1;
+
+        Vec3 start = START_POINT_STORAGE.get(source.getTextName());
+        int spherical = sphericalDistance(start, pos);
+        Messenger.send(source, findDistanceBetweenTwoPoints(start, pos));
+        return spherical;
     }
 }
