20c13c988d10a87b300e429a51f9c873931fd47a
Sriram Nair <srirambnair@gmail.com>
2026-01-02 20:34:15 -0500

    Editable Player NBT

--- src/main/java/carpet/mixins/EntityMixin.java
+++ src/main/java/carpet/mixins/EntityMixin.java
@@ -1,40 +1,92 @@
 package carpet.mixins;
 
+import carpet.CarpetSettings;
 import carpet.fakes.EntityInterface;
 import carpet.patches.EntityPlayerMPFake;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.network.protocol.game.ClientboundSetPassengersPacket;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
-public abstract class EntityMixin implements EntityInterface
-{
+public abstract class EntityMixin implements EntityInterface {
     @Shadow
     public float yRot;
-    
     @Shadow
     public float yRotO;
+    @Shadow
+    public Level level;
 
-    @Shadow public @Nullable abstract LivingEntity getControllingPassenger();
+    @Shadow
+    public @Nullable
+    abstract LivingEntity getControllingPassenger();
 
-    @Shadow public Level level;
+    @Final
+    private Entity self = (Entity) (Object) this;
 
     @Override
-    public float getMainYaw(float partialTicks)
-    {
+    public float getMainYaw(float partialTicks) {
         return partialTicks == 1.0F ? this.yRot : Mth.lerp(partialTicks, this.yRotO, this.yRot);
     }
 
     @Inject(method = "isLocalInstanceAuthoritative", at = @At("HEAD"), cancellable = true)
-    private void isFakePlayer(CallbackInfoReturnable<Boolean> cir)
-    {
-        if (getControllingPassenger() instanceof EntityPlayerMPFake) cir.setReturnValue(!level.isClientSide);
+    private void isFakePlayer(CallbackInfoReturnable<Boolean> cir) {
+        if (getControllingPassenger() instanceof EntityPlayerMPFake)
+            cir.setReturnValue(!level.isClientSide);
+    }
+
+    @Inject(method = "removePassenger", at = @At("TAIL"))
+    private void removePassengerForce(Entity passenger, CallbackInfo ci) {
+        if (!CarpetSettings.editablePlayerNbt) return;
+
+        if (!passenger.level().isClientSide && passenger instanceof ServerPlayer sp)
+            sp.connection.send(new ClientboundSetPassengersPacket(passenger));
+
+        if (!self.level().isClientSide && self instanceof ServerPlayer sp)
+            sp.connection.send(new ClientboundSetPassengersPacket(self));
+    }
+
+    @Inject(method = "addPassenger", at = @At("TAIL"))
+    private void addPassengerForce(Entity passenger, CallbackInfo ci) {
+        if (!CarpetSettings.editablePlayerNbt) return;
+
+        if (!passenger.level().isClientSide && passenger instanceof ServerPlayer sp)
+            sp.connection.send(new ClientboundSetPassengersPacket(passenger));
+
+        if (!self.level().isClientSide && self instanceof ServerPlayer sp)
+            sp.connection.send(new ClientboundSetPassengersPacket(self));
+    }
+
+    @WrapOperation(
+            method = "startRiding(Lnet/minecraft/world/entity/Entity;Z)Z",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/EntityType;canSerialize()Z"
+            )
+    )
+    private boolean allowPlayerRiding(
+            EntityType<?> type,
+            Operation<Boolean> original
+    ) {
+        if (!CarpetSettings.editablePlayerNbt)
+            return original.call(type);
+
+        if (type == EntityType.PLAYER)
+            return true;
+
+        return original.call(type);
     }
 }
